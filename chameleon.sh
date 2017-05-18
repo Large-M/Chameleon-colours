1ef2c0a2e4f12f1ec52f8b42f67acd1c52e4d50e
tiledman <migdfwoop@gmail.com>
2017-05-18 10:23:41 +0100

    Added informative output text via echo

--- chameleon.sh
+++ chameleon.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 cd colors
-./colors -n 16 $1 | ./script.sh >> ~/.Xresources
-sed -i 's/*/urxvt./g' ~/.Xresources
+echo "Generating colour scheme..."
+./colors -n 16 $1 | ./script.sh >> ~/Documents/Chameleon/.Xresources
+
+
+echo "Adding colour scheme to Xresources..."
+sed -i 's/*/urxvt./g' ~/Documents/Chameleon/.Xresources
+
+echo "Merging Xresources..."
 xrdb -merge ~/.Xresources
+echo "Done"
