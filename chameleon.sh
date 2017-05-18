9b5ceb5e10a77fe5b47469ade3be337c8e1de6b3
tiledman <migdfwoop@gmail.com>
2017-05-18 12:39:19 +0100

    Started to work on colour schemes for a variety of terminal emulators

--- chameleon.sh
+++ chameleon.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 cd colors
 echo "Generating colour scheme..."
-./colors -n 16 $1 | ./script.sh >> ~/Documents/Chameleon/.Xresources
+./colors -n 16 $1 | ./script.sh >> ../urxvt.txt
 
 
 echo "Adding colour scheme to Xresources..."
-sed -i 's/*/urxvt./g' ~/Documents/Chameleon/.Xresources
+sed -i 's/*/urxvt./g' ../urxvt.txt
 
 echo "Merging Xresources..."
 xrdb -merge ~/.Xresources
