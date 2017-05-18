f74e4f1838ed713d3b037f925c277eada6214022
tiledman <migdfwoop@gmail.com>
2017-05-18 10:10:37 +0100

    Used sed to replace *s in terminal output with urxvt.

--- chameleon.sh
+++ chameleon.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 cd colors
-./colors -n 16 $1 | ./script.sh >> ../output.txt | sed -i "s/\*/urxvt\./g"
+./colors -n 16 $1 | ./script.sh >> ../.Xresources
+sed -i 's/*/urxvt./g' ~/Documents/Chameleon/.Xresources
