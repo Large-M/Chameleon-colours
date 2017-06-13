e1685968b56e0b1fa79a998d9a5b47bcae1a91ee
tiledman <migdfwoop@gmail.com>
2017-06-13 11:33:05 +0100

    Fixed Xresourses.txt issue

--- chameleon.sh
+++ chameleon.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 cd colors
 echo "Generating colour scheme..."
-./colors -n 16 $1 | ../urxvt.sh >> ../xresourses.txt
+./colors -n 16 $1 | ./xresourses.sh >> ../xresourses.txt
 
 
 echo "Adding colour scheme to Xresourses.txt"
-sed -i 's/*/urxvt./g' ../xresourses.txt
+#sed -i 's/*./*/g' ../xresourses.txt
